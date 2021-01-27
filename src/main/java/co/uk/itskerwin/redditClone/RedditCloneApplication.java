f06294283f774685de240b3bb0e4ff50a4f74fd0
Will Kerwin <will@itskerwin.co.uk>
2021-01-27 16:54:03 +0000

    implemented swagger 2 documentation for api and endpoints

--- src/main/java/co/uk/itskerwin/redditClone/RedditCloneApplication.java
+++ src/main/java/co/uk/itskerwin/redditClone/RedditCloneApplication.java
@@ -1,11 +1,14 @@
 package co.uk.itskerwin.redditClone;
 
+import co.uk.itskerwin.redditClone.confg.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class RedditCloneApplication {
 
 	public static void main(String[] args) {
